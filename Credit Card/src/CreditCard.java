adab5cc54ff3a6028bc0abfb0f9fab1c62c734ce
liam.shaw <liam.shaw@MU-C1-06.mullen.pvt.k12.co.us>
2018-09-13 08:09:23 -0600

    started creditcard validator

---
+++ Credit Card/src/CreditCard.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class CreditCard
+	{
+		static long [] creditCardNumber = new long[16];
+		public static void main(String[] args)
+			{
+				
+			}
+		public static void askUserForACardNumber()
+			{
+				Scanner userInput = new Scanner(System.in);
+				System.out.println("Enter a credit card number");
+				long number = userInput.nextInt();
+				
+			}
+
+	}
