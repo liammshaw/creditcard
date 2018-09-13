62be47aa285574fceefcdac6e8d3df52db60e302
liam.shaw <liam.shaw@MU-C1-06.mullen.pvt.k12.co.us>
2018-09-13 08:57:34 -0600

    finished Credit Card Validator

--- Credit Card/src/CreditCard.java
+++ Credit Card/src/CreditCard.java
@@ -4,14 +4,35 @@ public class CreditCard
 		static long [] creditCardNumber = new long[16];
 		public static void main(String[] args)
 			{
-				
+				askUserForACardNumber();
+				alternatingDigits();
 			}
 		public static void askUserForACardNumber()
 			{
 				Scanner userInput = new Scanner(System.in);
 				System.out.println("Enter a credit card number");
-				long number = userInput.nextInt();
-				//trying to commit
+				long number = userInput.nextLong();
+				for(int i = 15; i >= 0; i--)
+					{
+						creditCardNumber [i] = number % 10;
+						number = number/10;
+					}
+			}
+		public static void alternatingDigits()
+			{
+				long firstDigit = 0; 
+				long lastDigit = 0;
+				for(int i = 0; i < 16; i = i +2) 
+					{
+						creditCardNumber[i] = creditCardNumber[i] *2;
+						if (creditCardNumber[i] >= 10)
+							{
+								firstDigit = creditCardNumber[i]%10;
+								lastDigit = creditCardNumber[i]/10;
+								creditCardNumber[i]= firstDigit + lastDigit;
+							}
+						System.out.println(creditCardNumber[i]);
+					}
 			}
 
 	}
